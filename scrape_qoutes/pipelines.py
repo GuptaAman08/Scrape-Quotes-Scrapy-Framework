727b8edd5a57cfd312484bce550c8688769c7825
Aman Gupta <aman.gupta@tacto.in>
2020-05-15 17:43:38 +0530

    adding pipe to filter and save data in SqliteDB and MongoDB

--- scrape_qoutes/pipelines.py
+++ scrape_qoutes/pipelines.py
@@ -11,9 +11,50 @@ from pymongo import MongoClient
 
 
 class ScrapeQoutesPipeline(object):
+    
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+
+
+    def create_connection(self):
+        self.conn = sqlite3.connect("quotes.db")
+        self.cur = self.conn.cursor()
+
+
+    def create_table(self):
+        # Drop the table if already existcls
+        self.cur.execute("""DROP TABLE IF EXISTS quotes_tb""")
+
+        self.cur.execute("""
+                    create table quotes_tb(
+                        title text,
+                        author text,
+                        tag text
+                    )
+                """)
+
+
+    def store_date(self, item):
+        self.cur.execute("""
+                insert into quotes_tb values (?,?,?)""",(item["title"][0], item["author"][0], item["tag"][0])
+            )
+
+        self.conn.commit()
 
     def process_item(self, item, spider):
         self.store_date(item)
         # print(item["title"])
         return item
 
+
+# Use Below code for storing in mongoDB
+class ScrapeQuotes(object):
+    def __init__(self):
+        self.conn = MongoClient("mongodb://localhost:27017/")
+        self.db = self.conn["qoutesDB"]
+        self.collection = self.db["qoutes_table"]
+
+    def process_item(self, item, spider):
+        self.collection.insert(dict(item))
+        return item
